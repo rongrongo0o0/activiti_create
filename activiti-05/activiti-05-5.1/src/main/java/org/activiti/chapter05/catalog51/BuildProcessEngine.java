e00c72781d0920a4f965d37bbd1f8de0d9fbe646
yangrong <731747116@qq.com>
2019-06-05 15:41:27 +0800

    chapter05

---
+++ activiti-05/activiti-05-5.1/src/main/java/org/activiti/chapter05/catalog51/BuildProcessEngine.java
@@ -0,0 +1,13 @@
+package org.activiti.chapter05.catalog51;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+
+public class BuildProcessEngine {
+	public static void main(String[] args) {
+		ProcessEngineConfiguration configuration = ProcessEngineConfiguration
+				.createProcessEngineConfigurationFromResource("build_engine.xml");
+		// 启动流程引擎
+		ProcessEngine engine= configuration.buildProcessEngine();
+	}
+}
---
+++ activiti-05/activiti-05-5.2/src/main/java/org/activiti/chapter05/catalog52/Init.java
@@ -0,0 +1,18 @@
+package org.activiti.chapter05.catalog52;
+
+import java.util.Map;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+
+public class Init {
+	public static void main(String[] args) {
+		ProcessEngines.init();// 
+		Map<String, ProcessEngine> map = ProcessEngines.getProcessEngines();
+		System.out.println(map.size());
+		System.out.println(map.get("default"));// 默认ProcessEngine实例名称
+		ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+		String name  =processEngine.getName();
+		System.out.println(name);
+	}
+}
